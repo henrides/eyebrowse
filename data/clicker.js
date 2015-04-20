eb8be11e9d6fca229ced96ba111336262be93d84
Marc-Olivier Mercier <henri_des@hotmail.com>
2015-04-20 09:20:17 -0400

    Working clicker
    Also rename TetApi.js to tet-api.js

--- data/clicker.js
+++ data/clicker.js
@@ -3,11 +3,62 @@
  * All rights reserved
  */
 $(function () {
+    var snapDistance = 50;
     var toggleTabbable = false;
+    var $currentElem = null;
+    var $tabbableElems = null
+
+	eyeBrowse.registerPositionListener(function (position) {
+        if (!toggleTabbable) {
+            return;
+        }
+
+        var $closestElem = null;
+        var closestElemDist = null;
+        $tabbableElems.each(function () {
+            var rect = this.getBoundingClientRect();
+            var region = {
+                position: { top: rect.top, left: rect.left },
+                dimension: { height: rect.height, width: rect.width }
+            };
+            var dist = eyeBrowse.getDistanceToRegion(position, region);
+            if ($closestElem === null || dist < closestElemDist) {
+                $closestElem = $(this);
+                closestElemDist = dist;
+            }
+        });
+
+        if (closestElemDist === null || closestElemDist > snapDistance) {
+            $closestElem = null;
+        }
+
+        if ($currentElem !== $closestElem) {
+            if ($currentElem !== null) {
+                $currentElem.removeClass('eb-clicker-hover');
+            }
+            if ($closestElem !== null) {
+                $closestElem.addClass('eb-clicker-hover');
+            }
+            $currentElem = $closestElem;
+        }
+    });
+
     $('body').keydown(function(key){
         if (key.altKey && key.ctrlKey) {
             toggleTabbable = !toggleTabbable;
-            window.console.log('toggle = ' + toggleTabbable);
+            if (!toggleTabbable) {
+                if ($currentElem !== null) {
+                    $currentElem.removeClass('eb-clicker-hover');
+                    var evt = new MouseEvent('click', {
+                        'bubbles': true,
+                        'cancelable': false
+                    });
+                    $currentElem[0].dispatchEvent(evt);
+                    $currentElem = null;
+                }
+            } else {
+                $tabbableElems = $(':tabbable');
+            }
             $(':tabbable').toggleClass('eb-clicker-tabbable', toggleTabbable);
         }
     });
